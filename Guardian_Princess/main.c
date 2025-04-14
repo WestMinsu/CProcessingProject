a5f59b06c13c2e64af5860c51e99934fcbabb549
WestMinsu <55283371+WestMinsu@users.noreply.github.com>
2025-04-14 11:37:12 +0900

    Update main.c

--- Guardian_Princess/main.c
+++ Guardian_Princess/main.c
@@ -9,15 +9,3 @@ int main(void)
 	CP_Engine_Run();
 	return 0;
 }
-
-#include "cprocessing.h"
-#include "mainmenu.h"
-#include "asset_loading.h"
-#include "utils.h"
-int main(void)
-{
-	CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
-	CP_System_SetWindowSize(1600, 900);
-	CP_Engine_Run();
-	return 0;
-}
